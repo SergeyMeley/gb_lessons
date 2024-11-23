230e188781d3937fab7b8428f36ccc28b610b48a
s.meleychuk@yandex.ru <s.meleychuk@yandex.ru>
2024-11-23 18:58:08 +0300

    Выполнил задание 3

--- 3 Introduction_to_programming_languages/homeworks/lesson7. Recursion/Task1/Program.cs
+++ 3 Introduction_to_programming_languages/homeworks/lesson7. Recursion/Task1/Program.cs
@@ -1,6 +1,31 @@
-﻿// Задача 1: Задайте значения M и N. Напишите
+﻿ // Задача 1: Задайте значения M и N. Напишите
 // программу, которая выведет все натуральные числа
 // в промежутке от M до N. Использовать рекурсию, не
 // использовать циклы.
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 5, 6, 7, 8"
+Console.WriteLine("Введите первое число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Результат:");
+WriteNumbers(firstNumber, secondNumber);
+
+void WriteNumbers(int firstNum, int secondNum)
+{
+    if(firstNum < secondNum)
+    {
+        Console.Write($"{firstNum}, ");
+        WriteNumbers(firstNum + 1, secondNum);
+    }
+    else if (firstNum > secondNum)
+    {
+        Console.Write($"{firstNum}, ");
+        WriteNumbers(firstNum - 1, secondNum);
+    }
+    else
+    {
+        Console.Write($"{firstNum}");
+        return;
+    } 
+}
