7d8ab2327e25e713acf20a5a7e1f534a02d7b83f
s.meleychuk@yandex.ru <s.meleychuk@yandex.ru>
2024-11-23 19:00:17 +0300

    Отформатировал код

--- 3 Introduction_to_programming_languages/homeworks/lesson7. Recursion/Task1/Program.cs
+++ 3 Introduction_to_programming_languages/homeworks/lesson7. Recursion/Task1/Program.cs
@@ -4,6 +4,7 @@
 // использовать циклы.
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
 Console.WriteLine("Введите первое число:");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
