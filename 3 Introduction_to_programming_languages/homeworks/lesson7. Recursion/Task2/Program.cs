04fe3cd07dffc3e3f8060d517c2e749c75c60fe0
s.meleychuk@yandex.ru <s.meleychuk@yandex.ru>
2024-11-23 14:36:59 +0300

    Выполнил задание 2

--- 3 Introduction_to_programming_languages/homeworks/lesson7. Recursion/Task2/Program.cs
+++ 3 Introduction_to_programming_languages/homeworks/lesson7. Recursion/Task2/Program.cs
@@ -2,3 +2,22 @@
 // Аккермана с помощью рекурсии. Даны два
 // неотрицательных числа m и n. 
 // m = 2, n = 3 -> A(m,n) = 29
+
+Console.Write(Ackerman(3, 2));
+
+int Ackerman(int n, int m)
+{
+
+    if(n == 0)
+    {
+        return m + 1;
+    }
+    else if(m == 0)
+    {
+        return Ackerman(n - 1, 1);
+    }
+    else
+    {
+        return Ackerman(n - 1, Ackerman(n, m - 1));
+    }
+}
