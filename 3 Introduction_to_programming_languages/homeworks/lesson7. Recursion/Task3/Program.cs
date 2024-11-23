7bf2426298a680cef75cfc0ee23fcc0d93ecfbcd
s.meleychuk@yandex.ru <s.meleychuk@yandex.ru>
2024-11-23 14:16:29 +0300

    Выполнил задание 3

--- 3 Introduction_to_programming_languages/homeworks/lesson7. Recursion/Task3/Program.cs
+++ 3 Introduction_to_programming_languages/homeworks/lesson7. Recursion/Task3/Program.cs
@@ -2,3 +2,16 @@
 // его элементы, начиная с конца. Использовать
 // рекурсию, не использовать циклы.
 // [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+int[] array = { 1, 2, 5, 0, 10, 34 };
+WriteReverceArray(array, 0);
+
+void WriteReverceArray(int[] arr, int index)
+{
+    if(index == arr.Length)
+    {
+        return;
+    }
+    WriteReverceArray(arr, index + 1);
+    Console.Write($"{arr[index]} ");
+}
